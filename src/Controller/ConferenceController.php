9a385632e75cbb12d1fa6c422170a1f99dce704d
AlexKlimovv <drummer1308@gmail.com>
2022-02-05 23:02:42 +0200

    5

---
+++ src/Controller/ConferenceController.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class ConferenceController extends AbstractController
+{
+    /**
+     * @Route("/", name="home page")
+     */
+    public function index(): Response
+    {
+        return new Response(<<<EOF
+<html>
+    <body>
+        <img src="public/images/under-construction.gif" />
+    </body>
+</html>
+EOF
+        );
+    }
+}
